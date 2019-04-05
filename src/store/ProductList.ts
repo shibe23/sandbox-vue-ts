76c3106ab5e21c4f7d6de1770a1fda16c7af4535
shibe23 <shibe23.info@gmail.com>
2019-04-06 00:06:12 +0900

    Add vue-type-helper

--- src/store/ProductList.ts
+++ src/store/ProductList.ts
@@ -1,28 +1,43 @@
 import { fetchProducts } from "../api";
-
-interface State {
+import {
+  DefineActions,
+  DefineGetters,
+  DefineMutations
+} from "vuex-type-helper";
+export interface State {
   products: Array<{
-    id: number
-    name: string
-    price: number
-    stock: number 
-  }>
+    id: number;
+    name: string;
+    price: number;
+    stock: number;
+  }>;
+}
+
+export interface Getters {
+  getAllProducts: {};
+}
+
+export interface Mutations {
+  SET_ITEMS: State
+}
+export interface Actions {
+  FETCH_PRODUCTS: void
 }
 
 export const state: State = {
   products: []
 };
-export const getters = {
+export const getters: DefineGetters<Getters, State> = {
   getAllProducts: state => {
     return state.products;
   }
 };
-export const mutations = {
+export const mutations: DefineMutations<Mutations, State> = {
   SET_ITEMS: (state, payload) => {
     state.products = payload.products;
   }
 };
-export const actions = {
+export const actions: DefineActions<Actions, State, Mutations, Getters> = {
   FETCH_PRODUCTS: async ({ commit }) => {
     const res = await fetchProducts();
     commit("SET_ITEMS", res.data);
